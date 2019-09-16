70ad79290c0f996e6e745e7e314acafcb06cdabb
BrandonPazRoman <55107456+BrandonPazRoman@users.noreply.github.com>
2019-09-16 15:06:18 -0400

    Add files via upload

---
+++ Examen Practico. Brandon Paz.cpp
@@ -0,0 +1,24 @@
+// Examen Practico.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "conio.h"
+using namespace std;
+void main ();
+{
+int n,aprobados,reprobados,suma,mayor,menor,promedio, abandonaron;
+float promedio = >10, <51;
+	do{
+cout <<endl;
+cout<< "1.- estudiantes que aprobaron" <<endl;
+cout<< "2.- estudiantes que reprobaron" <<endl;
+cout<< "3.- estudiantes que abandonaron" <<endl;
+
+		
+
+	return 0;
+
+	getch;}
+}
+
